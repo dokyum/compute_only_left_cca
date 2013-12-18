a0dcc509618d0ab8575f1888ae1267a10309ae77
dok027 <dok027@dk.(none)>
2013-12-17 17:18:20 -0800

    minor

--- stub.m
+++ stub.m
@@ -4,10 +4,10 @@ corpus_path = '/home/dok027/dataset/rcv1/list_from_rcv1_vocab300k_all_by_karl_cc
 %dict_path = '/home/dok027/dataset/rcv1/dict_from_rcv1_vocab300k_all';
 dict_path = '/home/dok027/dataset/rcv1/dict_from_rcv1_vocab300k_all_by_karl_cca';
 result_path = '/home/dok027/new_brown/cca_embedding/temp';
-context_specifier = 'lr2';
+context_specifier = 'r1';
 kappa = 1000;
-k = 1000;
+k = 3;
 
-only_left_cca(corpus_path, dict_path, context_specifier, kappa, k, result_path);
+only_left_cca(corpus_path, context_specifier, kappa, k, result_path);
 
 
